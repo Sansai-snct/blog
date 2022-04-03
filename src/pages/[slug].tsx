b296e902d829496730f27a711a95e031f06ad913
sansai-SNCT <syunzo1115@icloud.com>
2022-04-03 15:23:36 +0900

    [update] 個別記事ページの実装

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,54 @@
+import { NextPage, InferGetStaticPropsType } from 'next';
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import { getAllPosts, getPostBySlug } from '../lib/api';
+import Reactmarkdown from 'react-markdown';
+import Layout from '../components/Layout/Layout';
+import ReactMarkdown from 'react-markdown';
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticPaths = async () => {
+  const posts = getAllPosts(['slug']);
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }: any) => {
+  const post = getPostBySlug(params.slug, ['slug', 'title', 'date', 'content']);
+
+  const content = await post.content;
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+};
+
+const Post: NextPage<Props> = ({ post }) => {
+  const router = useRouter();
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <div>
+      <Layout pageTitle={post.title}>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+      </Layout>
+    </div>
+  );
+};
+
+export default Post;
