c90c308389bb7752487f3909b4107c7ceb94a360
sansai-SNCT <syunzo1115@icloud.com>
2022-04-04 12:43:46 +0900

    [update] ボタンに関数を付与#3

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import React, { VFC } from 'react';
+import useSimpleDarkMode from '../../lib/useSimpleDarkMode';
 
 import { FaMoon } from 'react-icons/fa';
 import { FaSun } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 const Header: VFC = () => {
+  const { isDarkMode, toggle } = useSimpleDarkMode();
   return (
     <section>
       <div>
-        <h1 className='text-4xl md:text-6xl text-center'>
+        <h1 className='text-4xl md:text-6xl text-center dark:text-white'>
           <Link href='/'>
             <a>Sansai's blog</a>
           </Link>
@@ -16,9 +18,7 @@ const Header: VFC = () => {
 
         <ul className='flex justify-end text-xl md:text-4xl'>
           <li className='mr-2 md:mr-4'>
-            <button>
-              <FaMoon />
-            </button>
+            <button onClick={() => toggle()}>{isDarkMode ? <FaSun /> : <FaMoon />}</button>
           </li>
           <li className='mr-2 md:mr-4'>
             <a target='_blank' href='https://github.com/Sansai-snct' rel='noopener noreferrer'>
