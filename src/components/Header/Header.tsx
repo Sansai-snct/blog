66d1e73cdb6fd5d381cff4022aec0240e1831744
sansai-SNCT <syunzo1115@icloud.com>
2022-04-04 11:45:35 +0900

    [update] リンクの設置と使用タグの変更 #3

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,33 @@
 import Link from 'next/link';
 import React, { VFC } from 'react';
+
+import { FaMoon } from 'react-icons/fa';
+import { FaSun } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 const Header: VFC = () => {
   return (
-    <header>
-      <nav className=' text-center '>
-        <Link href='/'>
-          <a className='text-4xl md:text-6xl  font-noto'>Sansai's blog</a>
-        </Link>
-      </nav>
-    </header>
+    <section>
+      <div>
+        <h1 className='text-4xl text-center'>
+          <Link href='/'>
+            <a>Sansai's blog</a>
+          </Link>
+        </h1>
+
+        <ul className='flex justify-end text-4xl'>
+          <li className='mr-4'>
+            <button>
+              <FaMoon />
+            </button>
+          </li>
+          <li className='mr-4'>
+            <a target='_blank' href='https://github.com/Sansai-snct' rel='noopener noreferrer'>
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
