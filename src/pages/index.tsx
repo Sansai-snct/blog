55e4b2aea9b5e54c265bae0c3ac66c3219d3199a
sansai-SNCT <syunzo1115@icloud.com>
2022-04-05 22:04:07 +0900

    [update] 記事のカードを作成 #7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,15 +16,17 @@ const Home: NextPage<Props> = ({ allPosts }) => {
     <div>
       <Layout pageTitle='blog|TOP'>
         <div className='text-2xl text-center'>記事一覧</div>
-        <ul>
-          <li>
-            {allPosts.map((post) => (
-              <a href={post.slug} key={post.slug}>
-                {post.title}
-                {post.date}
-              </a>
-            ))}
-          </li>
+        <ul className='flex flex-wrap  w-full lg:w-3/12  md:mr-4 mb-4 mt-10'>
+          {allPosts.map((post) => (
+            <a href={post.slug} key={post.slug}>
+              <li className='relactive h-48 w-72 bg-white rounded-lg border border-gray-200 shadow-md'>
+                <ul className='text-center mt-10'>
+                  <li className='text-2xl'>{post.title} </li>
+                  <li>{post.date}</li>
+                </ul>
+              </li>
+            </a>
+          ))}
         </ul>
       </Layout>
     </div>
