942c00a980d9ae4ec48a4ee35ec808b1be98dc65
sansai-SNCT <syunzo1115@icloud.com>
2022-04-06 23:56:43 +0900

    [update] カードのサイズ調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,17 +16,20 @@ const Home: NextPage<Props> = ({ allPosts }) => {
     <div>
       <Layout pageTitle='blog|TOP'>
         <div className='text-2xl text-center'>記事一覧</div>
-        <ul className='flex  ml-20  md:mr-4 mb-4 mt-10'>
+        <ul className='grid  md:grid-cols-4 gap-4  md:mr-4 mb-4 mt-10'>
           {allPosts.map((post) => (
-            <li className='p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
-              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+            <li className='p-6  h-48 bg-whiterounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+              <h5 className='mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white'>
                 {post.title}
               </h5>
-              <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{post.date}</p>
+              <p className='mb-3 font-normal text-center  text-gray-700 dark:text-gray-400'>
+                {post.date}
+              </p>
+
               <a
                 href={post.slug}
                 key={post.slug}
-                className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                className='inline-flex items-center mx-20  md:mx-36 py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
               >
                 Read more
                 <svg
