ec7402b88bf462d1b6032db3cfa29a8a3a957758
sansai-SNCT <syunzo1115@icloud.com>
2022-04-06 23:04:33 +0900

    [update] 記事のカードの仕様を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,16 +16,33 @@ const Home: NextPage<Props> = ({ allPosts }) => {
     <div>
       <Layout pageTitle='blog|TOP'>
         <div className='text-2xl text-center'>記事一覧</div>
-        <ul className='flex flex-wrap  w-full lg:w-3/12  md:mr-4 mb-4 mt-10'>
+        <ul className='flex  ml-20  md:mr-4 mb-4 mt-10'>
           {allPosts.map((post) => (
-            <a href={post.slug} key={post.slug}>
-              <li className='relactive h-48 w-72 bg-white rounded-lg border border-gray-200 shadow-md'>
-                <ul className='text-center mt-10'>
-                  <li className='text-2xl'>{post.title} </li>
-                  <li>{post.date}</li>
-                </ul>
-              </li>
-            </a>
+            <li className='p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                {post.title}
+              </h5>
+              <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{post.date}</p>
+              <a
+                href={post.slug}
+                key={post.slug}
+                className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              >
+                Read more
+                <svg
+                  className='ml-2 -mr-1 w-4 h-4'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                    clipRule='evenodd'
+                  ></path>
+                </svg>
+              </a>
+            </li>
           ))}
         </ul>
       </Layout>
