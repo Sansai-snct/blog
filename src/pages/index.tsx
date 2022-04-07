a2c8c2dc1c88f23e81053f08e63ac06b8c5e46ab
sansai-SNCT <syunzo1115@icloud.com>
2022-04-07 11:55:47 +0900

    [update] カードの配置を修正#7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,20 +16,20 @@ const Home: NextPage<Props> = ({ allPosts }) => {
     <div>
       <Layout pageTitle='blog|TOP'>
         <div className='text-2xl text-center'>記事一覧</div>
-        <ul className='grid  md:grid-cols-4 gap-4  md:mr-4 mb-4 mt-10'>
+        <ul className='grid  justify-center grid-rows-2 lg:grid-cols-2 lg:gap-4   lg:ml-72 lg:mr-72 mb-4 mt-10'>
           {allPosts.map((post) => (
-            <li className='p-6  h-48 bg-whiterounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <li className='p-6  h-48 bg-white rounded-lg border border-gray-200 shadow-md  dark:bg-gray-800 dark:border-gray-700'>
               <h5 className='mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white'>
                 {post.title}
               </h5>
               <p className='mb-3 font-normal text-center  text-gray-700 dark:text-gray-400'>
                 {post.date}
               </p>
-              <div className='block text-center'>
+              <div className='block text-center '>
                 <a
                   href={post.slug}
                   key={post.slug}
-                  className='inline-flex items-center  py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  className='inline-flex items-center  mt-4 py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                 >
                   Read more
                   <svg
