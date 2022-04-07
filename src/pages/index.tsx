8ea9c884a93bddd6311ea590295821636ba18acc
sansai-SNCT <syunzo1115@icloud.com>
2022-04-07 11:12:03 +0900

    [update] リンクボタンが中央に来るよう修正#7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,26 +25,27 @@ const Home: NextPage<Props> = ({ allPosts }) => {
               <p className='mb-3 font-normal text-center  text-gray-700 dark:text-gray-400'>
                 {post.date}
               </p>
-
-              <a
-                href={post.slug}
-                key={post.slug}
-                className='inline-flex items-center mx-20  md:mx-36 py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-              >
-                Read more
-                <svg
-                  className='ml-2 -mr-1 w-4 h-4'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
+              <div className='block text-center'>
+                <a
+                  href={post.slug}
+                  key={post.slug}
+                  className='inline-flex items-center  py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                 >
-                  <path
-                    fillRule='evenodd'
-                    d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                    clipRule='evenodd'
-                  ></path>
-                </svg>
-              </a>
+                  Read more
+                  <svg
+                    className='ml-2 -mr-1 w-4 h-4'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                      clipRule='evenodd'
+                    ></path>
+                  </svg>
+                </a>
+              </div>
             </li>
           ))}
         </ul>
