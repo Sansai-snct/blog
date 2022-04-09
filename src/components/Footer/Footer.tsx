d6978d0ed5460c80a5cb7a8241442bc0587b2ab1
sansai-SNCT <syunzo1115@icloud.com>
2022-04-09 17:20:53 +0900

    [update] ダークモード時のテキストカラーの追加#10

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React, { VFC } from 'react';
 const Footer: VFC = () => {
   return (
     <footer className='flex-grow'>
-      <p className='p-2 text-center text-xs bottom-0'>
+      <p className='p-2 text-center text-xs bottom-0 dark:text-white'>
         Copyright © 2022 Shunta Yamagiwa All Rights Reserved.
       </p>
     </footer>
