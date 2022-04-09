bb689323502e7c5073c96cc92a4dfc480b9cfe17
sansai-SNCT <syunzo1115@icloud.com>
2022-04-09 23:54:52 +0900

    [update] アイコンとタイトルのサイズ調整

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,13 +10,13 @@ const Header: VFC = () => {
   return (
     <section>
       <div>
-        <h1 className='text-4xl md:text-6xl text-center dark:text-white'>
+        <h1 className='text-3xl md:text-6xl text-center dark:text-white'>
           <Link href='/'>
             <a>Sansai's blog</a>
           </Link>
         </h1>
 
-        <ul className='flex justify-end text-xl md:text-4xl'>
+        <ul className='flex  justify-end text-2xl md:text-4xl'>
           <li className='mr-2 md:mr-4'>
             <button onClick={() => toggle()}>
               {isDarkMode ? <FaSun className='dark:text-white' /> : <FaMoon />}
