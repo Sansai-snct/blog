47fa02156a3c7c4f61cfe1a60954cc2eed87cbf1
sansai-SNCT <syunzo1115@icloud.com>
2022-05-26 23:45:25 +0900

    [Update] 本文の左右に余白を追加#32

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -46,9 +46,9 @@ const Post: NextPage<Props> = ({ post }) => {
   return (
     <div>
       <Layout pageTitle={post.title}>
-        <div className='grid grid-cols-9'>
+        <div className='grid grid-cols-8'>
           <div className='col-span-2' />
-          <div className='markdown-body col-span-10 md:col-span-5 m-10  dark:bg-gray-700 dark:text-white'>
+          <div className='markdown-body col-span-4 m-10  dark:bg-gray-700 dark:text-white'>
             <ReactMarkdown remarkPlugins={[gfm]}>{post.content}</ReactMarkdown>
           </div>
           <div className='col-span-2' />
